fc6ee2d11c1f235f8804d385a943c109b16ee211
Furimo-Virtual <mr.emeraldgame@gmail.com>
2021-01-22 02:10:24 +0200

    Reduced amount of junk variables inside Lesson2/Task3; Optimised variable initialization by improving data check inside loop.

--- Lesson_2/Task_3.cpp
+++ Lesson_2/Task_3.cpp
@@ -4,20 +4,13 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
     int endarray = -1;
     int arraynumbers[20];
     int loop = 0;
-=======
-    int endarray = 0;
-    int arraynumbers[20];
-    int input = 0;
->>>>>>> 5b83837... add task_3
 
     for(int i = 0; i < 20; i++){
         arraynumbers[i] = 0;
     }
-<<<<<<< HEAD
 
     do{
         endarray++;
@@ -25,19 +18,6 @@ int main()
         cin >> arraynumbers[endarray];
     }while(endarray < 20 && arraynumbers[endarray] > 0);
 
-=======
-    for(int i = 0; i < 20; i++){
-        do{
-            cout << "Enter number: " << endl;
-            cin >> arraynumbers[i];
-        }while(input < 0);
-
-        if(arraynumbers[i] == 0){
-            endarray = i;
-            break;
-        }
-    }
->>>>>>> 5b83837... add task_3
     for (int i = 0; i < endarray; i++){
         for(int spaces = 0; spaces <= ((endarray - arraynumbers[i] + 1)/2); spaces++){
             cout << " ";
