1a1b9cc567eb51ef2687158071362a1d9eca6656
Furimo-Virtual <mr.emeraldgame@gmail.com>
2021-01-25 13:08:30 +0200

    Added Lesson_3/Task_1.cpp

---
+++ Lesson_3/Task_1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    int size = 256;
+    char str[size];
+    int counter = 0;
+    int loop = 0;
+    bool cond1, cond2;
+
+    cout << "Enter string: ";
+    cin.getline(str,size);
+
+    while(str[loop] !='\0'){
+        cond1 = ((str[loop] >= 'A') && (str[loop] <= 'Z')) || ((str[loop] >= 'a') && (str[loop] <= 'z'));
+        cond2 = !(((str[loop+1] >= 'A') && (str[loop+1] <= 'Z')) || ((str[loop+1] >= 'a') && (str[loop+1] <= 'z')));
+        if(cond1 && cond2){
+            counter++;
+        }
+        loop++;
+    }
+    cout << counter;
+}
