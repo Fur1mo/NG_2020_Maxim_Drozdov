bf257fc147a55272d3a7e0c768dc72577f9cefca
Furimo-Virtual <mr.emeraldgame@gmail.com>
2021-01-26 12:34:42 +0200

    Variables have been renamed to make the code easier to read, and unnecessary parts of the code have been removed in Lesson_3/Task_3.cpp

--- Lesson_3/Task_3.cpp
+++ Lesson_3/Task_3.cpp
@@ -7,17 +7,17 @@ int main()
     int size = 256;
     char str[size];
     bool cond1;
-    int loop = 0;
+    int counter = 0;
 
     cout << "Enter string: ";
     cin.getline(str,size);
 
-    while(str[loop] != '\0'){
-        cond1 = ((str[loop] >= 'A') && (str[loop] <= 'Z')) || ((str[loop] >= 'a') && (str[loop] <= 'z'));
-        if(str[loop]>90 && cond1){
-            str[loop] = str[loop]-32;
+    while(str[counter] != 0){
+        cond1 = ((str[counter] >= 'a') && (str[counter] <= 'z'));
+        if(cond1){
+            str[counter] = str[counter]-32;
         }
-        loop++;
+        counter++;
     }
-    cout << str;
+    cout << str << endl;
 }
